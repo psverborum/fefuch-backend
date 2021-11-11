56b73df396e1bd5c9b2298b2451e633ad96bfab8
ubersenpaii <nyan4garyj@gmail.com>
2021-11-11 16:39:26 +0000

    Update simple post controller

--- src/main/java/com/verborum/fefuchbackend/Controller/PostController.java
+++ src/main/java/com/verborum/fefuchbackend/Controller/PostController.java
@@ -1,21 +1,45 @@
 package com.verborum.fefuchbackend.Controller;
 
+import com.verborum.fefuchbackend.Entity.Post;
+import com.verborum.fefuchbackend.Repository.PostRepository;
+import org.json.JSONArray;
 import org.json.JSONObject;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
+@RequestMapping(path = "/post")
 public class PostController {
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		Map<String, String> map = new HashMap<>();
-		map.put("name", name);
-		return JSONObject.valueToString(map);
+	private final PostRepository postRepository;
+
+	public PostController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
+
+	@PostMapping(path = "/add")
+	public @ResponseBody ResponseEntity<Post> addPost(@RequestBody Post post) {
+		postRepository.save(post);
+
+		return new ResponseEntity<>(post, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/all")
+	public @ResponseBody ResponseEntity<Iterable<Post>> getAllPosts() {
+		return new ResponseEntity<>(postRepository.findAll(), HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/{id}")
+	public @ResponseBody ResponseEntity<Post> getOne(@PathVariable Long id) {
+		Optional<Post> optPost = postRepository.findById(id);
+
+		return optPost.map(post -> new ResponseEntity<>(post, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.I_AM_A_TEAPOT));
 	}
 
 }
