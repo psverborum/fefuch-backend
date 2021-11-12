da393447926e940ee5b34575a71aaee9fa4c653b
ubersenpaii <nyan4garyj@gmail.com>
2021-11-12 13:19:00 +0000

    0.1.1 Add IP detector

--- src/main/java/com/verborum/fefuchbackend/Controller/PostController.java
+++ src/main/java/com/verborum/fefuchbackend/Controller/PostController.java
@@ -4,8 +4,12 @@ import com.verborum.fefuchbackend.Entity.Post;
 import com.verborum.fefuchbackend.Repository.PostRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 
 @RestController
@@ -19,7 +23,8 @@ public class PostController {
 	}
 
 	@PostMapping(path = "/add")
-	public @ResponseBody ResponseEntity<Post> addPost(@RequestBody Post post) {
+	public @ResponseBody ResponseEntity<Post> addPost(@RequestBody String text) {
+		Post post = new Post(fetchClientIpAddress(), text);
 		postRepository.save(post);
 
 		return new ResponseEntity<>(post, HttpStatus.OK);
@@ -27,7 +32,7 @@ public class PostController {
 
 	@GetMapping(path = "/all")
 	public @ResponseBody ResponseEntity<Iterable<Post>> getAllPosts() {
-		return new ResponseEntity<>(postRepository.findAll(), HttpStatus.OK);
+		return new ResponseEntity<>(postRepository.findAll(), HttpStatus.CREATED);
 	}
 
 	@GetMapping(path = "/{id}")
@@ -35,7 +40,18 @@ public class PostController {
 		Optional<Post> optPost = postRepository.findById(id);
 
 		return optPost.map(post -> new ResponseEntity<>(post, HttpStatus.OK))
-				.orElseGet(() -> new ResponseEntity<>(null, HttpStatus.I_AM_A_TEAPOT));
+				.orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NO_CONTENT));
+	}
+
+	@SuppressWarnings("ConstantConditions")
+	protected String fetchClientIpAddress() {
+		HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes()))
+				.getRequest();
+		String ip = Optional.ofNullable(request.getHeader("X-FORWARDED-FOR")).orElse(request.getRemoteAddr());
+		if (ip.equals("0:0:0:0:0:0:0:1"))
+			ip = "127.0.0.1";
+		Assert.isTrue(ip.chars().filter($ -> $ == '.').count() == 3, "Illegal IP: " + ip);
+		return ip;
 	}
 
 }
