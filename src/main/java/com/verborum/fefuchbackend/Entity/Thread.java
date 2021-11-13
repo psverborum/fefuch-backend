fb2f650528d7fa8f0cc742edede81b85a241a690
ubersenpaii <nyan4garyj@gmail.com>
2021-11-13 10:05:01 +0000

    0.1.1 Fix thread

--- src/main/java/com/verborum/fefuchbackend/Entity/Thread.java
+++ src/main/java/com/verborum/fefuchbackend/Entity/Thread.java
@@ -1,55 +1,80 @@
 package com.verborum.fefuchbackend.Entity;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class Thread {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "id", insertable=false, updatable=false)
-    @Column(nullable = false)
-    private User user;
+	@ManyToOne
+	@JoinColumn(name = "id", insertable = false, updatable = false)
+	private User user;
 
-    @Column(nullable = false)
-    private String text;
+	@Column(nullable = false)
+	private String text;
 
-    @Column(nullable = false)
-    private boolean enabled;
+	@Column(nullable = false)
+	private boolean enabled;
 
-    public Long getId() {
-        return id;
-    }
+    @OneToMany(mappedBy = "thread")
+    private List<Post> posts;
 
-    public User getUser() {
-        return user;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Post setUser(User user) {
-        this.user = user;
+	public User getUser() {
+		return user;
+	}
+
+	public Thread setUser(User user) {
+		this.user = user;
+
+		return this;
+	}
+
+	public String getText() {
+		return enabled ? text : "<<<пост удалён>>>";
+	}
+
+	public Thread setText(String text) {
+		this.text = text;
+
+		return this;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public Thread setEnabled(boolean enabled) {
+		this.enabled = enabled;
 
         return this;
     }
 
-    public String getText() {
-        return enabled ? text : "<<<пост удалён>>>";
+    public List<Post> getPosts() {
+        return posts;
     }
 
-    public Post setText(String text) {
-        this.text = text;
+    public Thread setPosts(List<Post> posts) {
+        this.posts = posts;
 
         return this;
     }
 
-    public boolean isEnabled() {
-        return enabled;
+    public Thread addPost(Post post) {
+        posts.add(post);
+
+        return this;
     }
 
-    public Post setEnabled(boolean enabled) {
-        this.enabled = enabled;
+    public Thread removePost(Post post) {
+        posts.remove(post);
 
         return this;
     }
